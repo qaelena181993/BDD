5eea7f53401978d0edee41030f373dba4580eb07
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-10-23 02:11:42 +0300

    Initial commit

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,39 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardNumber {
+       private String card;
+    }
+    public static CardNumber getFirstCardNumber() {
+        return new CardNumber("5559 0000 0000 0001");
+    }
+
+    public static CardNumber getSecondCardNumber() {
+        return new CardNumber("5559 0000 0000 0002");
+    }
+}
